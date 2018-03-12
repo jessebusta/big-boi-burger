6be3d59184270faeb381ce9f971514eda8881b5e
jbusta93 <jbusta93@gmail.com>
2018-03-11 18:30:43 -0700

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('big boi', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bigger boi', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('biggest boi', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('ALL CAPS BOI', false);
